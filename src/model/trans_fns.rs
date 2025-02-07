ed23f44244de3d2ab26ac411beabe9ae40863a6e
jr2bg <j.arturo.b.g@gmail.com>
2025-02-07 06:30:39 -0600

    Include all the transition functions for both models

--- src/model/trans_fns.rs
+++ src/model/trans_fns.rs
@@ -120,6 +120,25 @@ pub fn q2rf(pers: &mut Pers, config: &Config) {
     pers.set_p_state(State::I);
 }
 
+// seisf transition
+pub fn i2sf(pers: &mut Pers, config: &Config) {
+    let rand_numb: f32 = thread_rng().gen::<f32>();
+
+    if rand_numb <= config.case_fat_risk {
+        pers.set_state(State::F);
+        pers.set_t_state(0);
+    } else if rand_numb <= get_p_r(pers.t_state) + config.case_fat_risk {
+    //} else if rand_numb <= 0.25 + config.case_fat_risk {
+        // cambiar a cero o 1 el t_state
+        pers.set_state(State::S);
+        pers.set_t_state(0);
+    } else {
+        pers.add_time_state(1);
+    }
+    pers.set_p_state(State::I);
+}
+
+// seisf transition
 pub fn r2s(pers: &mut Pers, config: &Config) {
     let rand_numb: f32 = thread_rng().gen();
     if rand_numb <= config.p_S && pers.t_state >= config.t_S {
